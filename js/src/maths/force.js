27611a6f1161a5b8b2d02a0048ecf6886d922fb8
David Erwin <davidjerwin@gmail.com>
2015-02-17 15:56:51 -0500

    Adding 3 more force equations

--- js/src/maths/force.js
+++ js/src/maths/force.js
@@ -42,6 +42,88 @@ pg.maths.force = {
         }
     },
 
+    votesWithPartyBySenority: {
+        id: "votesWithPartyBySenority",
+        name: "Grid",
+        label: "Just a grid",
+        algorithm: function (o, i) {
+
+            var rowSpacing = 20,
+                foc, ofoc,
+                xPos = rowSpacing * o.get('seniority'),
+                k = 0.5;  // linear velocity
+            //  k = 1 * this.e.alpha; // decreasing velocity (easing);
+
+            if (! o.get('x')) { o.set('x', 0); }
+            if (! o.get('y')) { o.set('y', 0); }
+
+            o.get('party') == "D" ? foc = 0 : foc = 1;
+            o.get('party') == "D" ? ofoc = 1 : ofoc = 0;
+
+                // pull down for missed votes
+            o.set('y', o.get('y') + (xPos - o.get('y')) * k);
+
+
+            if (o.get('votes_with_party_pct') > 0.01) {
+                    // pull toward own party
+                o.set('x', o.get('x') + (this.foci[foc].x - o.get('x')) * k * (o.get('votes_with_party_pct') / 100));
+
+                    // pull toward other party
+                o.set('x', o.get('x') + (this.foci[ofoc].x - o.get('x')) * k * (1 - o.get('votes_with_party_pct') / 100) * 1);
+            } else {
+                o.set('x', o.get('x') + (this.foci[foc].x - o.get('x')) * k);
+            }
+
+            o.x = o.get('x');
+            o.y = o.get('y');
+
+        },
+        links: function () {
+            return [];
+        }
+
+    },
+    votesWithPartyBydw: {
+        id: "votesWithPartyBydw",
+        name: "Votes with party by DW Nominate",
+        label: "Just a grid",
+        algorithm: function (o, i) {
+
+            var rowSpacing = 20,
+                foc, ofoc,
+                xPos = rowSpacing * o.get('seniority'),
+                k = 0.5;  // linear velocity
+            //  k = 1 * this.e.alpha; // decreasing velocity (easing);
+
+            if (! o.get('x')) { o.set('x', 0); }
+            if (! o.get('y')) { o.set('y', 0); }
+
+            o.get('party') == "D" ? foc = 0 : foc = 1;
+            o.get('party') == "D" ? ofoc = 1 : ofoc = 0;
+
+                // pull down for missed votes
+            o.set('y', o.get('y') + (xPos - o.get('y')) * k);
+
+
+            if (o.get('votes_with_party_pct') > 0.01) {
+                    // pull toward own party
+                o.set('x', o.get('x') + (this.foci[foc].x - o.get('x')) * k * (o.get('votes_with_party_pct') / 100));
+
+                    // pull toward other party
+                o.set('x', o.get('x') + (this.foci[ofoc].x - o.get('x')) * k * (1 - o.get('votes_with_party_pct') / 100) * 1);
+            } else {
+                o.set('x', o.get('x') + (this.foci[foc].x - o.get('x')) * k);
+            }
+
+            o.x = o.get('x');
+            o.y = o.get('y');
+
+        },
+        links: function () {
+            return [];
+        }
+
+    },
     idealPoint: {
         id:"idealPoint",
         name: "Ideal Point",
@@ -51,6 +133,8 @@ pg.maths.force = {
             var foc, ofoc,
                 k = 0.5;  // linear velocity
             //  k = 1 * this.e.alpha; // decreasing velocity (easing);
+
+            console.log(i);
 
             if (! o.get('x')) { o.set('x', 0); }
             if (! o.get('y')) { o.set('y', 0); }
@@ -68,6 +152,28 @@ pg.maths.force = {
         }
     },
 
+
+    grid: {
+        id: "grid",
+        name: "Grid",
+        label: "Just a grid",
+        algorithm: function (o, i) {
+
+            var spacing = 50,
+                cols = Math.floor(this.width / spacing) - 1;
+
+            o.x = (i % cols) * spacing + spacing;
+            o.y = Math.floor(i / cols) * spacing + spacing;
+
+            o.set('x', o.x);
+            o.set('y', o.y);
+
+        },
+        links: function () {
+            return [];
+        }
+
+    },   
     accurateRepresentation: {
         id: "accurateRepresentation",
         name: "Accurate",
@@ -79,5 +185,7 @@ pg.maths.force = {
             return [];
         }
     }
+
+
 
 };
